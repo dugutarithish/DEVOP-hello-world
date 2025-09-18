805780f6c5162a652af23d3c3262901a4f857ebd
dugutarithish <146820750+dugutarithish@users.noreply.github.com>
2025-09-19 01:26:40 +0530

    Update app.py

--- myproject/app.py
+++ myproject/app.py
@@ -1,3 +1,16 @@
-mkdir hello-world
-$ cd hello-world
-$ python3 -m venv .venv
+python
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+    
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+  if __name__ == "__main__":
+    app.run(host"0.0.0.0" , port=5000)
